9378f000cb7c9b08afe5d3a8ba71c640c9c107ec
Timilehin <olutimilehinolayinka1234@gmail.com>
2023-06-29 20:47:14 +0100

    added the name of the app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,10 @@ import { NavLink } from "react-router-dom"
 
 const Footer = () => {
   return (
-    <div className="flex px-[6em] py-[4em] gap-[5em] text-black font-Rubik">
-      <p>Logo</p>
+    <div className="flex px-[6em] py-[4em] gap-[5em] text-black font-Rubik items-center">
+      <p className="text-3xl text-customPink">
+      ¢ℓєνяя
+      </p>
       <div className="flex gap space-x-[3em] ">
         <NavLink to='/'>Home</NavLink>
         <NavLink to='/buyaccount'>Buy Accounts</NavLink>
